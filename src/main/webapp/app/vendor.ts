1ccdc38ad682ed3b30f2bf9e496e94ec9042893b
huangzhaorongit <huangzhaorongit@gmail.com>
2017-11-22 10:13:28 +0800

    ngx-admin

--- src/main/webapp/app/vendor.ts
+++ src/main/webapp/app/vendor.ts
@@ -1,4 +1,12 @@
 /* after changing this file run 'yarn run webpack:build' */
 /* tslint:disable */
 import '../content/scss/vendor.scss';
+import "tinymce/tinymce.min.js";
+import "tinymce/themes/modern/theme.min.js";
+import "tinymce/plugins/link/plugin.min.js";
+import "tinymce/plugins/paste/plugin.min.js";
+import "tinymce/plugins/table/plugin.min.js";
+import "chart.js/dist/Chart.min.js";
+import "./ngx-admin/assets/vendors/echarts.world.min.js";
+
 // jhipster-needle-add-element-to-vendor - JHipster will add new menu items here
