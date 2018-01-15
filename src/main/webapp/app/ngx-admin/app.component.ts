e38c8a494ece9f8be7d6ec80132fa3c448c403da
lumin <lumin@hxxn.onaliyun.com>
2018-01-15 16:58:55 +0800

    修改JHIPSTER表格

--- src/main/webapp/app/ngx-admin/app.component.ts
+++ src/main/webapp/app/ngx-admin/app.component.ts
@@ -11,7 +11,7 @@ import {ApiService} from "./app.service";
   selector: 'ngx-app',
   template: `
       <router-outlet></router-outlet>
-     
+      <router-outlet name="popup"></router-outlet>
   `,
 })
 export class AppComponent implements OnInit {
