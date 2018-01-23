aa7a939815aab83b8841f1674911e10957cb93aa
huangzhaorongit <huangzhaorongit@gmail.com>
2018-01-23 16:07:32 +0800

    添加认证拦截器

--- src/main/webapp/app/ngx-admin/pages/pages.module.ts
+++ src/main/webapp/app/ngx-admin/pages/pages.module.ts
@@ -1,32 +1,41 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { PagesComponent } from './pages.component';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { PagesRoutingModule } from './pages-routing.module';
-import { ThemeModule } from '../@theme/theme.module';
+import {PagesComponent} from './pages.component';
+import {DashboardModule} from './dashboard/dashboard.module';
+import {PagesRoutingModule} from './pages-routing.module';
+import {ThemeModule} from '../@theme/theme.module';
 import {JhiEventManager} from "ng-jhipster";
 import {EmCloudWebAdminModule} from "./admin/admin.module";
+import {ProfileService} from "../../layouts/profiles/profile.service";
+import {customHttpProvider} from "../../blocks/interceptor/http.provider";
+import {PaginationConfig} from "../../blocks/config/uib-pagination.config";
+import {UserRouteAccessService} from "../../shared/auth/user-route-access-service";
 
 
 const PAGES_COMPONENTS = [
-  PagesComponent,
+    PagesComponent,
 
 ];
 
 @NgModule({
-  imports: [
-    PagesRoutingModule,
-    ThemeModule,
-    DashboardModule,
+    imports: [
+        PagesRoutingModule,
+        ThemeModule,
+        DashboardModule,
 
-      //增加docs模块
-      EmCloudWebAdminModule
+        //增加docs模块
+        EmCloudWebAdminModule
 
-  ],
-  declarations: [
-    ...PAGES_COMPONENTS,
-  ],
-    providers:[JhiEventManager]
+    ],
+    declarations: [
+        ...PAGES_COMPONENTS,
+    ],
+    providers: [JhiEventManager,
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService,
+    ]
 })
 export class PagesModule {
 }
